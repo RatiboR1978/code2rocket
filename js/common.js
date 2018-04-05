461a1ad561a0247e6c42398720b3948419c21ce0
Ratibor <klov29@mail.ru>
2018-04-05 16:43:46 +0700

    Сверстал reviews, начал footer

--- js/common.js
+++ js/common.js
@@ -15,7 +15,18 @@ $(function () {
         slidesToScroll: 1
     });
 
+    $('.reviews__slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        arrows: false,
+        dots: true,
+        slidesToScroll: 1
+    });
 
+    $('.advantages__item-title').on('click', function () {
+        $(this).next('.advantages__hidden').toggleClass('advantages__text');
+        $(this).toggleClass('advantages__item-title--active');
+    })
 
 
 
