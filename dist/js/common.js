bb8e428a53adeba1bd5f7a9d300001235db400d2
Ratibor <klov29@mail.ru>
2018-04-09 14:58:13 +0700

    Сделал header через grid

--- dist/js/common.js
+++ dist/js/common.js
@@ -1,8 +1,27 @@
+'use strict';
+
 $(function () {
+    $('.courses__slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        arrows: false,
+        dots: true,
+        slidesToScroll: 1
+    });
 
+    $('.teachers__slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        arrows: false,
+        dots: true,
+        slidesToScroll: 1
+    });
 
-
-
-
-
+    $('.reviews__slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        arrows: false,
+        dots: true,
+        slidesToScroll: 1
+    });
 });
